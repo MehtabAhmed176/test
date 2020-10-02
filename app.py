296149c78037e66efa178c3b809183e6fb6771bc
Mehtab Ahmed <mehtab.ahmed@stud.th-deg.de>
2020-10-02 22:22:35 +0200

    adding new lh

--- app.py
+++ app.py
@@ -3,42 +3,11 @@ from flask import request, jsonify
 from flask import render_template
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+
+
 app = flask.Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
-
-@app.route('/jobscore', methods=['POST'])
-def file():
-
-    # Store the resume in a variable
-    resume =request.form['ResumeData']
-    jd=request.form['JD']
-    print(resume)
-    print(jd)
-
-    #Create a list of resume and job descrition
-    text = [resume, jd]
-
-
-    #Create a count vectorizer object and convert the text documents to a matrix of token counts.
-    cv = CountVectorizer()
-
-    #Something
-    count_matrix = cv.fit_transform(text)
-    print("\nSimilarity Scores:")
-    print(cosine_similarity(count_matrix))
-
-    #get the match percentage
-    matchPercentage = cosine_similarity(count_matrix)[0][1] * 100
-    matchPercentage = round(matchPercentage, 2) # round to two decimal
-    print("Your resume matches about "+ str(matchPercentage)+ "% of the job description.")
-
-    return str(matchPercentage)
-
-
-app.run()
+@app.route('/', methods=['GET'])
+def home():
+    return "I am home page and just sucked"
