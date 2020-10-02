cc59849de6a0b5ee589a210e7c99ea3c45df8dda
Mehtab Ahmed <mehtab.ahmed@stud.th-deg.de>
2020-10-02 21:45:13 +0200

    adding new page

--- app.py
+++ app.py
@@ -1,21 +1,44 @@
 import flask
-from flask import request
-import pandas as pd
-from datetime import datetime as dt
-
-data = pd.read_csv('data.csv', names=['s', 'e', 'm']).set_index('m')
-
-series = pd.Series(index=range(data.s.min(), dt.now().year + 1))
-for m in data.index:
-    series.loc[data.loc[m].s:data.loc[m].e] = m
-
+from flask import request, jsonify
+from flask import render_template
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 app = flask.Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def home():
-    year = int(request.args['year'])
-    try:
-        return series.loc[year]
-    except KeyError:
-        return f'Invalid input ({series.index.min()} - {series.index.max()})'
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+
+@app.route('/jobscore', methods=['POST'])
+def file():
+
+    # Store the resume in a variable
+    resume =request.form['ResumeData']
+    jd=request.form['JD']
+    print(resume)
+    print(jd)
+
+    #Create a list of resume and job descrition
+    text = [resume, jd]
+
+
+    #Create a count vectorizer object and convert the text documents to a matrix of token counts.
+    cv = CountVectorizer()
+
+    #Something
+    count_matrix = cv.fit_transform(text)
+    print("\nSimilarity Scores:")
+    print(cosine_similarity(count_matrix))
+
+    #get the match percentage
+    matchPercentage = cosine_similarity(count_matrix)[0][1] * 100
+    matchPercentage = round(matchPercentage, 2) # round to two decimal
+    print("Your resume matches about "+ str(matchPercentage)+ "% of the job description.")
+
+    return str(matchPercentage)
+
+
+app.run()
