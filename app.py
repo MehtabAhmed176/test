518c01018047b3bbe45f8f00016f91ffeb24fdf0
Mehtab Ahmed <mehtab.ahmed@stud.th-deg.de>
2020-10-05 19:12:07 +0200

    adding try catch

--- app.py
+++ app.py
@@ -27,28 +27,30 @@ def home():
 
 @app.route('/jobscore', methods=['POST'])
 def file():
-
+    try:
     # Store the resume in a variable
-    resume =request.form['ResumeData']
-    jd=request.form['JD']
-    print(resume)
-    print(jd)
+        resume =request.form['ResumeData']
+        jd=request.form['JD']
+        print(resume)
+        print(jd)
 
-    #Create a list of resume and job descrition
-    text = [resume, jd]
+        #Create a list of resume and job descrition
+        text = [resume, jd]
 
 
-    #Create a count vectorizer object and convert the text documents to a matrix of token counts.
-    cv = CountVectorizer()
+        #Create a count vectorizer object and convert the text documents to a matrix of token counts.
+        cv = CountVectorizer()
 
-    #Something
-    count_matrix = cv.fit_transform(text)
-    print("\nSimilarity Scores:")
-    print(cosine_similarity(count_matrix))
+        #Something
+        count_matrix = cv.fit_transform(text)
+        print("\nSimilarity Scores:")
+        print(cosine_similarity(count_matrix))
 
-    #get the match percentage
-    matchPercentage = cosine_similarity(count_matrix)[0][1] * 100
-    matchPercentage = round(matchPercentage, 2) # round to two decimal
-    print("Your resume matches about "+ str(matchPercentage)+ "% of the job description.")
-
-    return str(matchPercentage)
+        #get the match percentage
+        matchPercentage = cosine_similarity(count_matrix)[0][1] * 100
+        matchPercentage = round(matchPercentage, 2) # round to two decimal
+        print("Your resume matches about "+ str(matchPercentage)+ "% of the job description.")
+        return str(matchPercentage)
+    
+    except:
+           print("Please provide a valid data")
